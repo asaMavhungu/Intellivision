1fe54e86a613a429888233ee62e26f4515ee0c8a
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-04-22 19:02:43 +0200

    Created train and test functions

--- intelliVision.py
+++ intelliVision.py
@@ -19,3 +19,41 @@ test_data = datasets.CIFAR10(
     transform=ToTensor(),
     download=True
     )
+
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.reshape(-1, 32*32*3).to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # BackProagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model):
+    size = len(dataloader.dataset)
+    model.eval() # WHAT DOES THIS DO?
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.reshape(-1,32*32*3).to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= size
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
