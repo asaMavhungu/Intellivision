102c1ed3b77fc95ccf2c7751e419a56e566c3ac8
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-04-22 19:17:23 +0200

    Increased learning rate: 1e-3 -> 1e-2

--- intelliVision.py
+++ intelliVision.py
@@ -86,7 +86,7 @@ model = NeuralNetwork().to(device)
 print(model)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)
 
 epochs = 10
 for t in range(epochs):
