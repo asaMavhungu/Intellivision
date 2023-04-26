37fb67dec69877fc518e56394b95b8f6e8dd4dbb
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-04-26 20:02:27 +0200

    finished making leNet5 implementation

--- cnn.py
+++ cnn.py
@@ -6,17 +6,57 @@ import torch.nn.functional as F # Activation Functions
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 5, 3, padding=0) # First Conv Layer
-        self.pool = nn.MaxPool2d(2)  # For pooling
-        self.flatten = nn.Flatten() # For flattening the 2D image
-        self.fc1 = nn.Linear(5*16*16, 120)  # First FC HL
-        self.fc2= nn.Linear(120, 10) # Output layer
+        # input = 3x32x32, output = 6x28x28
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=6,kernel_size=5, stride=1,padding=0)  
+        # input = 6x28x28, output = 6x14x14
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)  
+        # input = 6x14x14, output = 16x10x10
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)  
+        # input = 16x10x10, output = 16x5x5
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)  
+        # input = 16x5x5, output = 120
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  
+        self.fc2 = nn.Linear(120, 84)  
+        self.fc3 = nn.Linear(84, 10)  
 
     def forward(self, x):
-      # Batch x of shape (B, C, W, H)
-      x = F.relu(self.conv1(x)) # Shape: (B, 5, 32, 32)
-      x = self.pool(x)  # Shape: (B, 5, 16, 16)
-      x = self.flatten(x) # Shape: (B, 980)
-      x = F.relu(self.fc1(x))  # Shape (B, 256)
-      x = self.fc2(x)  # Shape: (B, 10)
-      return x  
+        x = F.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool2(x)
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+if __name__ == "__main__":
+    
+	import torch.optim as optim # Optimizers
+	from torch.optim.lr_scheduler import StepLR
+        
+	input_size = 32*32*3
+	hidden_size = [512*2, 512, 256]
+	output_size = 10
+        
+	cnn = CNN().to(device)
+	print(cnn)
+
+	LEARNING_RATE = 1.5e-2
+	MOMENTUM = 0.9
+	STEP_SIZE = 10  # adjust this to suit your needs
+	GAMMA = 0.1  # adjust this to suit your needs
+	DECAY = 0.001
+
+	print(f"lr={LEARNING_RATE}, m={MOMENTUM}, step={STEP_SIZE}, gamme={GAMMA}, decay={DECAY}")
+	# Define the loss function, optimizer, and learning rate scheduler
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.SGD(cnn.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=DECAY)
+	lr_scheduler = StepLR(optimizer, step_size=STEP_SIZE, gamma=GAMMA)
+
+	# Train the MLP for 5 epochs
+	for epoch in range(15):
+		train_loss = train(cnn, train_loader, criterion, optimizer, device)
+		test_acc = test(cnn, test_loader, device)
+		print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}, Learning rate = {optimizer.param_groups[0]['lr']:.4f}")
+		lr_scheduler.step()  # apply learning rate decay
