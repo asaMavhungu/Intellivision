4f635f9b04257488385af6c59e6568d5bd5d6206
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-04-27 23:20:52 +0200

    Added Type hints

--- mlp.py
+++ mlp.py
@@ -1,15 +1,15 @@
-from utils import *
+import utils
 
 import torch.nn as nn 
 
 class MLP(nn.Module):
-    def __init__(self, input_size, hidden_sizes, output_size, dropout_rate=0.5):
+    def __init__(self, input_size:int, hidden_sizes:list[int], output_size:int, dropout_rate:float =0.5) -> None:
         super(MLP, self).__init__()
         self.input_size = input_size
         self.output_size = output_size
         
-        layers = []
-        prev_size = input_size
+        layers: list[nn.Module] = []
+        prev_size: int = input_size
         for size in hidden_sizes:
             layers.append(nn.Linear(prev_size, size))
             layers.append(nn.ReLU())
@@ -18,9 +18,9 @@ class MLP(nn.Module):
         layers.append(nn.Linear(prev_size, output_size))
         self.linear_relu_stack = nn.Sequential(*layers)
 
-    def forward(self, x):
+    def forward(self, x: utils.torch.Tensor) -> utils.torch.Tensor:
         x = x.view(-1, self.input_size)
-        logits = self.linear_relu_stack(x)
+        logits: utils.torch.Tensor = self.linear_relu_stack(x)
         return logits
 
 if __name__ == "__main__":
@@ -28,32 +28,31 @@ if __name__ == "__main__":
 	import torch.optim as optim # Optimizers
 	from torch.optim.lr_scheduler import StepLR
         
-	input_size = 32*32*3
-	hidden_size = [512*2, 512, 256]
-	output_size = 10
+	input_size: int = 32*32*3
+	hidden_size: list[int] = [512*2, 512, 256]
+	output_size: int = 10
         
-	mlp = MLP(input_size, hidden_size, output_size ).to(device)
-	print(mlp)
-
-	
+	mlp = MLP(input_size, hidden_size, output_size ).to(utils.device)
+	print(type(mlp))
 
 	LEARNING_RATE = 1.5e-2
 	MOMENTUM = 0.9
-	STEP_SIZE = 10  # adjust this to suit your needs
-	GAMMA = 0.1  # adjust this to suit your needs
+	STEP_SIZE = 10
+	GAMMA = 0.1
 	DECAY = 0.001
 
 	hidden = [512*2, 512, 256]
 
 	print(f"lr={LEARNING_RATE}, m={MOMENTUM}, step={STEP_SIZE}, gamme={GAMMA}, decay={DECAY}")
+
 	# Define the loss function, optimizer, and learning rate scheduler
 	criterion = nn.CrossEntropyLoss()
 	optimizer = optim.SGD(mlp.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=DECAY)
 	lr_scheduler = StepLR(optimizer, step_size=STEP_SIZE, gamma=GAMMA)
 
-	# Train the MLP for 5 epochs
+	# Train the MLP for 15 epochs
 	for epoch in range(15):
-		train_loss = train(mlp, train_loader, criterion, optimizer, device)
-		test_acc = test(mlp, test_loader, device)
+		train_loss = utils.train(mlp, utils.train_loader, criterion, optimizer, utils.device)
+		test_acc = utils.test(mlp, utils.test_loader, utils.device)
 		print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}, Learning rate = {optimizer.param_groups[0]['lr']:.4f}")
 		lr_scheduler.step()  # apply learning rate decay
