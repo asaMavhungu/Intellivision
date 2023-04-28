3f04d8b8387fb44bbb805c780044fa2f157f2365
Asakundwi Mavhungu <mavhasa17@gmail.com>
2023-04-28 07:31:55 +0200

    Re-organized code and added main function

--- MLP.py
+++ MLP.py
@@ -42,21 +42,49 @@ class MLP(nn.Module):
 		x = x.view(-1, self.input_size)
 		logits = self.linear_relu_stack(x)
 		return logits
+	
+def main():
+	import torch.optim as optim
+	from torch.optim.lr_scheduler import StepLR
+
+	mlp = MLP(input_size, hidden_size, output_size ).to(utils.device)
+	print(mlp)
+
+	LEARNING_RATE = 1.5e-2
+	MOMENTUM = 0.9
+	STEP_SIZE = 8
+	GAMMA = 0.1
+	DECAY = 0.001
+
+	print(f"lr={LEARNING_RATE}, m={MOMENTUM}, step={STEP_SIZE}, gamme={GAMMA}, decay={DECAY}")
+
+	# Define the loss function, optimizer, and learning rate scheduler
+	criterion = nn.CrossEntropyLoss()
+	optimizer = optim.SGD(mlp.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=DECAY)
+	lr_scheduler = StepLR(optimizer, step_size=STEP_SIZE, gamma=GAMMA)
+
+	# Train the MLP for 15 epochs
+	for epoch in range(15):
+		train_loss = utils.train(mlp, utils.train_loader, criterion, optimizer, utils.device)
+		test_acc = utils.test(mlp, utils.test_loader, utils.device)
+		print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}, Learning rate = {optimizer.param_groups[0]['lr']:.4f}")
+		lr_scheduler.step()  # apply learning rate decay
+	
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "-save":
+			print("Saving model...")
+			utils.torch.save(mlp.state_dict(), "./mlp.pt")
+			print("Done!")
+
 
 if __name__ == "__main__":
 	
 	import sys
 	import os
 
-	import torch.optim as optim
-	from torch.optim.lr_scheduler import StepLR
-		
 	input_size: int = 32*32*3
 	hidden_size: list[int] = [512*2, 512, 256]
 	output_size: int = 10
-		
-	mlp = MLP(input_size, hidden_size, output_size ).to(utils.device)
-
 
 	if len(sys.argv) != 1 and len(sys.argv) != 2:
 		print("Invalid number of arguments. Usage: python MODEL_NAME.py [-load | -save]")
@@ -68,6 +96,7 @@ if __name__ == "__main__":
 		if os.path.isfile("./mlp.pt"):
 			print("Loading model...")
 			# load the model parameters
+			mlp = MLP(input_size, hidden_size, output_size ).to(utils.device)
 			mlp.load_state_dict(utils.torch.load("./mlp.pt"))
 			print("Done!")
 			test_acc = utils.test(mlp, utils.test_loader, utils.device)
@@ -75,34 +104,5 @@ if __name__ == "__main__":
 		else:
 			print("Saved model not found!")
 
-			
 	elif len(sys.argv) == 2 or len(sys.argv) == 1:
-		print(mlp)
-
-		LEARNING_RATE = 1.5e-2
-		MOMENTUM = 0.9
-		STEP_SIZE = 8
-		GAMMA = 0.1
-		DECAY = 0.001
-
-		hidden = [512*2, 512, 256]
-
-		print(f"lr={LEARNING_RATE}, m={MOMENTUM}, step={STEP_SIZE}, gamme={GAMMA}, decay={DECAY}")
-
-		# Define the loss function, optimizer, and learning rate scheduler
-		criterion = nn.CrossEntropyLoss()
-		optimizer = optim.SGD(mlp.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM, weight_decay=DECAY)
-		lr_scheduler = StepLR(optimizer, step_size=STEP_SIZE, gamma=GAMMA)
-
-		# Train the MLP for 15 epochs
-		for epoch in range(15):
-			train_loss = utils.train(mlp, utils.train_loader, criterion, optimizer, utils.device)
-			test_acc = utils.test(mlp, utils.test_loader, utils.device)
-			print(f"Epoch {epoch+1}: Train loss = {train_loss:.4f}, Test accuracy = {test_acc:.4f}, Learning rate = {optimizer.param_groups[0]['lr']:.4f}")
-			lr_scheduler.step()  # apply learning rate decay
-		
-		if len(sys.argv) == 2:
-			if sys.argv[1] == "-save":
-				print("Saving model...")
-				utils.torch.save(mlp.state_dict(), "./mlp.pt")
-				print("Done!")
+		main()
